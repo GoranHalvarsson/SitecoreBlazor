edeeff24b57c285d86b2d9fca4c03dda33de26eb
Göran Halvarsson <goran.halvarsson@stendahls.se>
2019-11-01 14:41:25 +0100

    Minor cleanups

--- SitecoreBlazorHosted.Client/Startup.cs
+++ SitecoreBlazorHosted.Client/Startup.cs
@@ -3,22 +3,17 @@ using Foundation.BlazorExtensions.Extensions;
 using Foundation.BlazorExtensions.Services;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Project.BlazorSite;
-
 
 namespace SitecoreBlazorHosted.Client
 {
-
 
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddSingleton<HttpClient>((s) => new HttpClient());
             services.AddScoped<IRestService, RestService>();
             services.AddForFoundationBlazorExtensions();
             services.AddForFeatureNavigation();
-
         }
 
         public void Configure(IComponentsApplicationBuilder app)
